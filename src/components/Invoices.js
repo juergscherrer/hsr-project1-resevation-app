983c0966476a51ea9e4d6e8d7a4f9f7bf7400d88
Markus Tobler <mail@markustobler.ch>
2018-11-02 13:55:43 +0100

    invoices with moment

---
+++ src/components/Invoices.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import withAuthorization from './UserAuthentication/withAuthorization';
+import { db } from '../firebase';
+
+class AdminPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: null,
+        };
+    }
+
+    componentDidMount() {
+        db.onceGetUsers().then(snapshot =>
+            this.setState({ users: snapshot.val() })
+        );
+    }
+
+    render() {
+        const { users } = this.state;
+
+        return (
+            <div>
+                <h1>Admin Dashboard</h1>
+                { !!users && <UserList users={users} /> }
+            </div>
+        );
+    }
+}
+
+const UserList = ({ users }) =>
+    <div>
+        <h2>Users</h2>
+
+        {Object.keys(users).map(key =>
+            <div key={key}>{users[key].firstname} {users[key].lastname}  {users[key].email}</div>
+        )}
+    </div>
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(AdminPage);
